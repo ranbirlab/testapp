09892c829a513f09422d8b7837b2e6b12426453b
Ranbir Negi <ranbirnegi@Ranbirs-MacBook-Air.local>
2023-05-08 20:50:52 +0530

    Add Terraform code and staging-tfvars.cfg for Azure IaaS Windows VM

--- variables.tf
+++ variables.tf
@@ -1,24 +1,20 @@
 variable "prefix" {
   description = "Prefix for resources"
   type        = string
-  default     = "example"
 }
 
 variable "location" {
   description = "Azure region to deploy the resources"
   type        = string
-  default     = "East US"
 }
 
 variable "admin_username" {
   description = "Administrator username for the VM"
   type        = string
-  default     = "azureuser"
 }
 
 variable "admin_password" {
   description = "Administrator password for the VM"
   type        = string
-  default     = "P@ssw0rd123!"
   sensitive   = true
 }
